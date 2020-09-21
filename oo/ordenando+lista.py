532aacae1152324c299dd2971ca2c9c5603bae6b
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-09-21 08:59:52 -0300

    Ordenando lista com for e if

---
+++ oo/ordenando+lista.py
@@ -0,0 +1,14 @@
+lista = [3,2,1]
+
+for i in range(len(lista)):
+	menor = i
+	for j in range(i+1,len(lista)):
+		if lista[j] == lista[menor]:
+			menor = j
+
+			if lista[i] != lista[menor]:
+				aux = lista[i]
+				lista[i] = lista[menor]
+				lista[menor] = aux
+
+print(lista)
