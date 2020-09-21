07347575b09f73dba107868d26259f57796ea74e
Vanderson de Castro <vanderson.Castro@ufv.br>
2020-09-21 09:42:28 -0300

    Ordenando lista com for e if

--- oo/ordenando+lista.py
+++ oo/ordenando+lista.py
@@ -1,14 +1,14 @@
-lista = [3,2,1]
+lista = [325,20,302,36,3,2,1]
 
 for i in range(len(lista)):
 	menor = i
-	for j in range(i+1,len(lista)):
-		if lista[j] == lista[menor]:
+	for j in range(i+1, len(lista)):
+		if lista[j] < lista[menor]:
 			menor = j
 
-			if lista[i] != lista[menor]:
-				aux = lista[i]
-				lista[i] = lista[menor]
-				lista[menor] = aux
+		if lista[i] != lista[menor]:
+			aux = lista[i]
+			lista[i] = lista[menor]
+			lista[menor] = aux
 
 print(lista)
